9375518de4687b12b2e79c8d372cf1926cc28f27
Emircan <emircandv787@gmail.com>
2024-11-12 02:22:56 +0300

    The primeicons has been added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
+import { PrimeIcons } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +14,12 @@ import { ButtonModule } from 'primeng/button';
     RouterOutlet,
     // BrowserModule,
     // BrowserAnimationsModule,
-    ButtonModule],
+    ButtonModule,
+    SidebarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'prime-ng';
+  sidebarVisible: boolean = false;
 }
